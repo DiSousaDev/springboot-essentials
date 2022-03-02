ff37722e75a3b044595ba69c8d44c07e985ae531
Eder Diego de Sousa <me@diego.dev.br>
2022-03-01 22:57:13 -0300

    delete anime

--- src/main/java/br/dev/diego/springbootessentials/controller/AnimeController.java
+++ src/main/java/br/dev/diego/springbootessentials/controller/AnimeController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,6 +46,12 @@ public class AnimeController {
     @PostMapping
     public ResponseEntity<Anime> save(@RequestBody Anime anime) {
         return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        animeService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
