b5ef7d78d42e091df7da8528dcda90a7f1b11138
Eder Diego de Sousa <me@diego.dev.br>
2022-03-01 23:29:48 -0300

    add mysql dependency and jpa

--- src/main/java/br/dev/diego/springbootessentials/domain/Anime.java
+++ src/main/java/br/dev/diego/springbootessentials/domain/Anime.java
@@ -1,7 +1,15 @@
 package br.dev.diego.springbootessentials.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Anime {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
