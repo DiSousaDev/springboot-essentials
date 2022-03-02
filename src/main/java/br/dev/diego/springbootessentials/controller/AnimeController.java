47aed60d6aba7a0e2722d7f199ab56a9c0f05d21
Eder Diego de Sousa <me@diego.dev.br>
2022-03-01 23:02:53 -0300

    put anime

--- src/main/java/br/dev/diego/springbootessentials/controller/AnimeController.java
+++ src/main/java/br/dev/diego/springbootessentials/controller/AnimeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,6 +52,12 @@ public class AnimeController {
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         animeService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> update(@RequestBody Anime anime) {
+        animeService.update(anime);
         return ResponseEntity.noContent().build();
     }
 
