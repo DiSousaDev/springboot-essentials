e9c61760f0366a4af46e036dd81e319cf25f6d66
Eder Diego de Sousa <me@diego.dev.br>
2022-03-05 00:59:17 -0300

    RestTemplate exchange

--- src/main/java/br/dev/diego/springbootessentials/client/SpringClient.java
+++ src/main/java/br/dev/diego/springbootessentials/client/SpringClient.java
@@ -3,12 +3,17 @@ package br.dev.diego.springbootessentials.client;
 import br.dev.diego.springbootessentials.entities.Anime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SpringClient {
 
-        public static final Logger LOG = LoggerFactory.getLogger(SpringClient.class);
+    public static final Logger LOG = LoggerFactory.getLogger(SpringClient.class);
 
     public static void main(String[] args) {
         ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/{id}", Anime.class, 2);
@@ -17,6 +22,14 @@ public class SpringClient {
         Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 3);
         LOG.info("ID :" + object.getId());
         LOG.info("Name :" + object.getName());
+
+        Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes/all", Anime[].class);
+        LOG.info(Arrays.toString(animes));
+
+        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all",
+                HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+        LOG.info(exchange.getBody().toString());
+
     }
 
 }
